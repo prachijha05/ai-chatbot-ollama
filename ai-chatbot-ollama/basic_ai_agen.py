7b6a86bfcf776fc40a04903a2e36204e6961e2f6
prachijha05 <prachijha9354@gmail.com>
2026-06-03 17:18:08 +0530

    Initial commit - AI chatbot using LangChain and Mistral

---
+++ ai-chatbot-ollama/basic_ai_agen.py
@@ -0,0 +1,17 @@
+from langchain_ollama import OllamaLLM
+
+# Load AI Model from Ollama
+llm = OllamaLLM(model="mistral")    # can use gemma or lamma 3 modle also
+
+print("\nWelcome to your AI Agent! Ask me anything.")
+
+while True:
+    question = input("Your Question (or type 'exit' to stop): ")
+
+    if question.lower() == "exit":
+        print("Goodbye!")
+        break
+
+    response = llm.invoke(question)
+
+    print("\nAI Response:", response)
